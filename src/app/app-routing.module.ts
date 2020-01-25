d72245db94b063ae1b2d491b2e5922e96bbac66f
Hubert <Psychol9827@gmail.com>
2020-01-25 20:21:08 +0100

    Revert "Merge branch 'master' of https://github.com/joannamaria27/BibliotekaAngular"
    This reverts commit 2f65ebe188c66c073757e7ab793342199e6f278d, reversing
    changes made to 05a9f7798ff7d55d637161025d1ab3022d80c071.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DodawanieStudentComponent } from './dodawanie-student/dodawanie-student.component'
-import { DodawanieAutorComponent } from './dodawanie-autor/dodawanie-autor.component'
-import { DodawanieKsiazkaComponent } from './dodawanie-ksiazka/dodawanie-ksiazka.component'
 
 
-const routes: Routes = [
-  { path: "student", component: DodawanieStudentComponent, pathMatch: 'full'},
-  { path: "autor", component: DodawanieAutorComponent, pathMatch: 'full'},
-  { path: "ksiazka", component: DodawanieKsiazkaComponent, pathMatch: 'full'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-
 })
 export class AppRoutingModule { }
