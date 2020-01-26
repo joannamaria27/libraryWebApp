63d427793e989825143122cdea5fd2b27334b3c6
joannamaria27 <joan.mk7@gmail.com>
2020-01-26 15:20:19 +0100

    add start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { DodawanieStudentComponent } from './dodawanie-student/dodawanie-student.component'
 import { DodawanieAutorComponent } from './dodawanie-autor/dodawanie-autor.component'
 import { DodawanieKsiazkaComponent } from './dodawanie-ksiazka/dodawanie-ksiazka.component'
+import { WypozyczeniaComponent } from './wypozyczenia/wypozyczenia.component';
 
 
 const routes: Routes = [
+  { path: "", component: WypozyczeniaComponent, pathMatch: 'full'},
   { path: "student", component: DodawanieStudentComponent, pathMatch: 'full'},
   { path: "autor", component: DodawanieAutorComponent, pathMatch: 'full'},
   { path: "ksiazka", component: DodawanieKsiazkaComponent, pathMatch: 'full'}
--- src/app/wypozyczenia/wypozyczenia.component.ts
+++ src/app/wypozyczenia/wypozyczenia.component.ts
@@ -7,14 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WypozyczeniaComponent implements OnInit {
 
-  optionsSelect: Array<any>;
+
   ngOnInit() {
-    this.optionsSelect = [
-      { value: 'Feedback', label: 'Feedback' },
-      { value: 'Report a bug', label: 'Report a bug' },
-      { value: 'Feature request', label: 'Feature request' },
-      { value: 'Other stuff', label: 'Other stuff' },
-    ];
+   
   }
 
 }
